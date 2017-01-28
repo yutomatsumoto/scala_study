1ebce071d1c7dd026f074fac83846f33135f73a6
mazinai_tarazu <mazinai.tarazu@gmail.com>
2017-01-28 13:00:44 +0900

    Add curry and file match

---
+++ src/main/scala/mazinai/Curry.scala
@@ -0,0 +1,19 @@
+package mazinai
+
+/**
+  * Created by mazinai_tarazu on 1/26/17.
+  */
+object Curry {
+
+  def oldSum(x: Int, y:Int) = x + y
+
+  def curriedSum(x: Int)(y: Int) = x + y
+
+  def first(x: Int) = (y: Int) => x + y
+
+  def curriedExe() = {
+    val second =first(1)
+    second(2)
+  }
+
+}
---
+++ src/main/scala/mazinai/FileMatch.scala
@@ -0,0 +1,24 @@
+package mazinai
+
+/**
+  * Created by mazinai_tarazu on 1/26/17.
+  */
+object FileMatch {
+
+  private val lists = List("aaa", "bbb", "ccc")
+
+//  def filesMatching(query: String,
+//                    matcher: (String, String) => Boolean) = {
+//    for (list <- lists; if matcher(list, query))
+//      yield list
+//  }
+
+  def filesMatching(matcher: String => Boolean) = {
+    for (list <- lists; if matcher(list))
+      yield list
+  }
+//  def listRegex(query: String) = filesMatching(query, _.matches(_))
+//  def listRegex(query: String) = filesMatching(query, (fileName: String, query: String) => fileName.matches(query))
+  def listRegex(query: String) = filesMatching(_.matches(query))
+
+}
