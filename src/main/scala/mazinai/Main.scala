4cb0d71faa4f5b8240f9cbf551897d5c8a3ceecd
mazinai-tarazu <mazinai.tarazu@gmail.com>
2017-01-30 17:27:01 +0900

    Add trait test

--- src/main/scala/mazinai/Main.scala
+++ src/main/scala/mazinai/Main.scala
@@ -5,31 +5,35 @@ object Main {
   def main(args: Array[String]): Unit = {
 
     //fizzbuzz
-//    Exam.fizzBuzz(927).foreach(println(_))
+    //    Exam.fizzBuzz(927).foreach(println(_))
 
     //primecheck
-//     println(Exam.primeCheck(824)
+    //     println(Exam.primeCheck(824)
 
     //calendar
-//    Exam.cal(2014, 5)
+    //    Exam.cal(2014, 5)
 
-//    val gameValues = Map("fallout4"->7800, "undertale"->980)
-//    print(Exam.mapTest(gameValues))
+    //    val gameValues = Map("fallout4"->7800, "undertale"->980)
+    //    print(Exam.mapTest(gameValues))
 
-//    val rational = new Rational(1, 2)
-//    val rational2 = new Rational(1, 3)
-//    println(rational + rational2)
+    //    val rational = new Rational(1, 2)
+    //    val rational2 = new Rational(1, 3)
+    //    println(rational + rational2)
 
-//    println(Exam.yieldTest(List("a", "bb", "ccc")))
+    //    println(Exam.yieldTest(List("a", "bb", "ccc")))
 
-//    val multiTable = new MultiplicationTable()
-//    print(multiTable.multiTable())
+    //    val multiTable = new MultiplicationTable()
+    //    print(multiTable.multiTable())
 
-    val cont = Array("aaa", "bbb")
-    val cont2 = Array("aaa", "bbb", "ccc")
-    val e1 = Element.elem(cont)
-    val e2 = Element.elem(cont2)
+    //    val cont = Array("aaa", "bbb")
+    //    val cont2 = Array("aaa", "bbb", "ccc")
+    //    val e1 = Element.elem(cont)
+    //    val e2 = Element.elem(cont2)
+    //
+    //    println(e1 above e2)
 
-    println(e1 above e2)
+    val myQue = new MyQueue2
+    myQue.put(1)
+    println(myQue.get())
   }
 }
